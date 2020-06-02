5d7a43af27b258f9c1ccf19e4bcbef277144977e
JeysoMelgar <47490817+JeysoMelgar@users.noreply.github.com>
2020-06-01 22:15:40 -0600

    Update Ejercicio_2.php

--- Ejercicio_2.php
+++ Ejercicio_2.php
@@ -15,20 +15,20 @@
 
     echo "<html><head><meta charset=\"utf-8\"></head>";
     echo "<body>";
-    echo "<h2>Esta es la clase de $NombreClase manana y tarde (Simbolo) </h2>" ;
+    echo "<h2>Esta es la clase de $NombreClase manana y tarde (Simbolo) </h2>";
     echo "<h2>Esta es otra fila de la clase de ".$NombreClase." con dos cadenas concatenadas </h2>";
     echo "</body>";
     echo "</html>";
 
     $numero = 50;
-    $calculo = $numero / 2+6;
+    $calculo = $numero / 2 + 6;
     echo "El resultado de la operacion es : $calculo";
 
     $logico = true;
     $logico2 = false;
     echo "<p> El contenido de una variable tipo booleano verdadero: $logico y variable boolena falsa:$logico2 </p>";
 
-    $variableNombreClase ="NombreClase"
+    $variableNombreClase ="NombreClase";
     echo "<p>Este es un nombre de variable extraido de una cadena".$$variableNombreClase."</p>";
 ?>
 
