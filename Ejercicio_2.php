665517d500d5b374808b4e67fdab60acf1e4e6c5
JeysoMelgar <47490817+JeysoMelgar@users.noreply.github.com>
2020-06-01 21:52:31 -0600

    Actualizando ejercicio 2

--- Ejercicio_2.php
+++ Ejercicio_2.php
@@ -2,6 +2,35 @@
 <html>
 <body>
 <h1>Ejercicio 2</h1>
+<?php
+    // Este es un comentario de una linea
+
+    /*
+        Este es 
+        Un comentario de multiples 
+        Lineas
+    */ 
+
+    $NombreClase ="Lenguaje 4";
+
+    echo "<html><head><meta charset=\"utf-8\"></head>";
+    echo "<body>";
+    echo "<h2>Esta es la clase de $NombreClase manana y tarde (Simbolo) </h2>" ;
+    echo "<h2>Esta es otra fila de la clase de ".$NombreClase." con dos cadenas concatenadas </h2>";
+    echo "</body>";
+    echo "</html>";
+
+    $numero = 50;
+    $calculo = $numero / 2+6;
+    echo "El resultado de la operacion es : $calculo";
+
+    $logico = true;
+    $logico2 = false;
+    echo "<p> El contenido de una variable tipo booleano verdadero: $logico y variable boolena falsa:$logico2 </p>";
+
+    $variableNombreClase ="NombreClase"
+    echo "<p>Este es un nombre de variable extraido de una cadena".$$variableNombreClase."</p>";
+?>
 
 </body>
 </html>
